adc1dd1cfac6e927e11bd202fa4a213ef36bc73a
Mel Holl <mel@lastgameboard.com>
2022-02-02 16:15:42 -0500

    Testing mock events

--- scripts/setup.js
+++ scripts/setup.js
@@ -4,7 +4,7 @@ Hooks.once('init', () => {
 	game.settings.register('gameboard', 'isOnGameboard', {
 		name: 'OnGameboard',
 		hint: 'True if the instance is running on a gameboard. False if running elsewhere.', 
-		default: false,
+		default: isOnGameboard,
 		type: Boolean,
 		scope: 'client',
 		config: false
@@ -19,13 +19,11 @@ Hooks.once('init', () => {
 		config: false
 	});
 
-    //Check if on gameboard or on web
     //On Web, show warning for larger image sizes / downscale too large of images
     //On gameboard
     //change UI
     //init token pairing logic
-    //change zoom level + disable zoom
-
+    //change zoom level + disable zoom (match grid size)
 });
 
 
@@ -34,3 +32,72 @@ Hooks.on("canvasInit", () => {
     SightLayer.MAXIMUM_FOW_TEXTURE_SIZE = 4096 / 2;
 });
 
+var tokenMoveEventMap = {};
+
+function onTokMessageReceived(tokMessage) {
+	console.log(tokMessage);
+	var actorMap = game && game.settings ? game.settings.get('gameboard', 'actorIdMap') : {}; 
+
+	var actorId = actorMap[tokMessage.typeId];
+
+	//Check if token is paired already, if so move token to location on board
+	//if(actorId) {
+		moveTokenToLocation(actorId, tokMessage);
+	// } else {
+	// 	pairToken(tokMessage);
+	// }
+}
+
+function moveTokenToLocation(actorId, tokMessage) {
+	if(tokenMoveEventMap[actorId] == tokMessage.sessionId) {
+		var evtMove = new MouseEvent("mousemove", {
+			view: window,
+			bubbles: true,
+			cancelable: true,
+			clientX: window.innerWidth * tokMessage.xPosition,
+			clientY: window.innerWidth * tokMessage.yPosition,
+		});
+		var evtUp = new MouseEvent("mouseUp", {
+			view: window,
+			bubbles: true,
+			cancelable: true,
+		});
+		$(document).dispatchEvent(evtMove);
+		$(document).dispatchEvent(evtUp);
+	} else {
+		tokenMoveEventMap[actorId] = tokMessage.sessionId;
+		var evtDown = new MouseEvent("mousedown", {
+			view: window,
+			bubbles: true,
+			cancelable: true,
+			clientX: window.innerWidth * tokMessage.xPosition,
+			clientY: window.innerWidth * tokMessage.yPosition,
+		});
+		$(document).dispatchEvent(evtDown);
+	}
+
+ 	//Move token (local vs pushing data)
+
+	// var canvasX = calculateCanvasPosition(tokMessage.xPosition); // TODO calculate location based on zoom/pan of canvas
+	// var canvasY = tokMessage.yPosition; 
+
+ 	// _token.setPosition(canvasX, canvasY);
+}
+
+// function calculateCanvasPosition(position){
+// 	var viewPosition = canvas.scene._viewPosition;
+// 	var scale = viewPosition.scale;
+// 	var width = canvas.dimensions.sceneWidth + canvas.dimensions.paddingX;
+// 	var height = canvas.dimensions.sceneHeight + canvas.dimensions.paddingY;
+
+// 	var topX = viewPosition.x - (width * (1 - scale)/2); 
+// 	var topY = viewPosition.y - (height * (1 - scale)/2); 
+
+// 	var bottomX = viewPosition.x + (width * (1 - scale)/2); 
+// 	var bottomY = viewPosition.y + (height * (1 - scale)/2); 
+// }
+
+function pairToken(tokMessage) {
+	//If not paired, trigger click on canvas and if actor is selected and not paired, pair
+	//If not paired and actor selected is paired, remove previous pairing and re-pair
+}
