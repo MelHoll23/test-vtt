0652f0d0b34e550df3ce974bb84c95d4be86c4c3
Mel Holl <mel@lastgameboard.com>
2022-02-08 15:29:13 -0500

    debugging

--- scripts/setup.js
+++ scripts/setup.js
@@ -22,7 +22,6 @@ Hooks.once('init', () => {
     //On Web, show warning for larger image sizes / downscale too large of images
     //On gameboard
     //change UI
-    //init token pairing logic
     //change zoom level + disable zoom (match grid size)
 });
 
@@ -35,31 +34,38 @@ Hooks.on("canvasInit", () => {
 
 var tokenMoveEventMap = {};
 
+var callsToOnTokMessageReceived = 0;
+var callsToMoveToken = 0;
+
 function onTokMessageReceived(tokMessage) {
-	console.log(tokMessage);
+	callsToOnTokMessageReceived++;
+	console.log("Message received", callsToOnTokMessageReceived);
+	console.log("Move token", callsToMoveToken);
+	//console.log(tokMessage);
 	var actorMap = game.settings.get('gameboard', 'actorIdMap');
 
 	var parsedTokMessage = JSON.parse(tokMessage)[0];//TODO handle array
 	var actorId = actorMap[parsedTokMessage.typeId];
 
-	console.log(actorId);
+	//console.log(actorId);
 	//Check if token is paired already, if so move token to location on board
 	if(actorId) {
-		moveTokenToLocation(actorId, parsedTokMessage); 
+		foundry.utils.debounce(moveTokenToLocation(actorId, parsedTokMessage), 1000); 
 	} else {
 		pairToken(actorMap, parsedTokMessage);
 	}
 }
 
 function moveTokenToLocation(actorId, tokMessage) {
+	callsToMoveToken++;
  	//Move token (local vs pushing data)
-	console.log("tok", tokMessage.positionX, tokMessage.positionY);
+	//console.log("tok", tokMessage.positionX, tokMessage.positionY);
 
 	var positions = calculateCanvasPosition(tokMessage.positionX, tokMessage.positionY);
 	var rotation = ((tokMessage.angle + 3) * 60) % 360;
 
- 	console.log("positions", positions.x, positions.y);
-	console.log("rotation", tokMessage.angle, rotation)
+ 	// console.log("positions", positions.x, positions.y);
+	// console.log("rotation", tokMessage.angle, rotation)
 
 	//canvas.grid.getSnappedPosition() //TODO snap after done moving for a while
 
@@ -75,31 +81,33 @@ function moveTokenToLocation(actorId, tokMessage) {
 }
 
 function calculateCanvasPosition(positionX, positionY){
-	console.log("tok x/y", positionX, positionY);
+	//console.log("tok x/y", positionX, positionY);
 	var viewPosition = canvas.scene._viewPosition;
 	var scale = viewPosition.scale;
 
-	console.log("viewPosition", JSON.stringify(viewPosition));
+	//console.log("viewPosition", JSON.stringify(viewPosition));
 
 	var canvasViewWidth =  window.innerWidth * (1 + scale);
 	var canvasViewHeight = window.innerHeight * (1 + scale);
 
-	console.log("canvasWidth/canvasHeight:", canvasViewWidth, canvasViewHeight);
+	//console.log("canvasWidth/canvasHeight:", canvasViewWidth, canvasViewHeight);
 
 	var topX = viewPosition.x - canvasViewWidth/2; 
 	var topY = viewPosition.y - canvasViewHeight/2; 
 
-	console.log("topx/y", topX, topY);
+	var topX = canvas.stage.transform.worldTransform
+
+	//console.log("topx/y", topX, topY);
 
 	var distanceDiffX = positionX * canvasViewWidth;
 	var distanceDiffY = positionY * canvasViewHeight;
 
-	console.log("diff", distanceDiffX, distanceDiffY);
+	//console.log("diff", distanceDiffX, distanceDiffY);
 
 	actualPositionX = topX + distanceDiffX;
 	actualPositionY = topY + distanceDiffY;
 
-	console.log("actualX, actualY",  actualPositionX, actualPositionY);
+	//console.log("actualX, actualY",  actualPositionX, actualPositionY);
 	return {x: actualPositionX, y: actualPositionY}
 	//return canvas.grid.getSnappedPosition(actualPositionX, actualPositionY, 1);
 }
@@ -108,16 +116,19 @@ function pairToken(actorMap, tokMessage) {
 	var positions = calculateCanvasPosition(tokMessage.positionX, tokMessage.positionY);
 	//If not paired, if actor is selected and not paired, pair
 	console.log("Trying to pair at", positions.x, positions.y);
-	console.log(canvas.tokens.ownedTokens.length);
+
 	canvas.tokens.ownedTokens.filter(owned => !Object.values(actorMap).includes(owned.data._id)).forEach((token) => {
 		let tokenPosition = new PIXI.Rectangle(token.x, token.y, token.w, token.h);
 		console.log(tokenPosition.x, tokenPosition.y);
 		if(tokenPosition.contains(positions.x, positions.y)) {
-			console.log("Paired!", token.data._id);
-			//pair token
+			//Unpair if needed
+			if(Object.values(actorMap).includes(token.data._id)){
+				delete actorMap[Object.keys(actorMap).find(key => actorMap[key] === token.data._id)]
+			}
+			//Pair token
 			actorMap[tokMessage.typeId] = token.data._id;
 			game.settings.set("gameboard", "actorIdMap", actorMap);
+			console.log("Paired!", token.data._id);
 		}
 	});
-	//If not paired and actor selected is paired, remove previous pairing and re-pair
 }
