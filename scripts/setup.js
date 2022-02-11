247c8d98aa7afa1abb17dfa5e9e8de097215df3e
Mel Holl <mel@lastgameboard.com>
2022-02-11 13:17:32 -0500

    Pass correct object

--- scripts/setup.js
+++ scripts/setup.js
@@ -7,7 +7,7 @@ window.onTokMessageReceived = function(tokMessage) {
 	var parsedTokMessages = JSON.parse(tokMessage);
 
 	parsedTokMessages.forEach(parsedTokMessage => {
-		var tokenMovementAdaptor = new TokenMovementAdaptor(tokMessage);
+		var tokenMovementAdaptor = new TokenMovementAdaptor(parsedTokMessage);
 
 		//Check if token is paired already, if so move token to location on board
 		if(tokenMovementAdaptor.shouldMoveToken()) {
