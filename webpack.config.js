df11bc3e3a88d8e0a94a239f6940b215d7a161cb
Melanie Holl <mel@lastgameboard.com>
2022-08-24 16:59:42 -0400

    Test update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const path = require('path')
+
 module.exports = {
     context: __dirname,
     mode: 'production',
     entry: './scripts/index.js',
+    output: {
+        filename: 'gameboard.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
     resolve: {
         extensions: ['.ts', '.js'],
         modules: ['scripts', 'node_modules']
