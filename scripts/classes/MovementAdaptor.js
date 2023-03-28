dcc409203b1d8853a2a5d1add91c4a5c36110c1e
Melanie Holl <mel@lastgameboard.com>
2023-03-28 16:48:31 -0400

    Attempt only update instead of setPosition

--- scripts/classes/MovementAdaptor.js
+++ scripts/classes/MovementAdaptor.js
@@ -33,14 +33,20 @@ export default class TokenMovementAdaptor {
         
         console.log('Gameboard | Move to', `${tokenCenteredPositions.x}, ${tokenCenteredPositions.y}`);
 
-        actor._object.setPosition(tokenCenteredPositions.x, tokenCenteredPositions.y, {recenter: false});
-        actor.data.update({
-            rotation: rotation
-        });
-        actor._object.updateSource(); //Updates local vision with rotation (token not rotated until saveMovement)
+        actor.update({
+            x: tokenCenteredPositions.x,
+            y:  tokenCenteredPositions.y,
+            rotation: rotation 
+        }, {recenter: false});
+
+        // actor._object.setPosition(tokenCenteredPositions.x, tokenCenteredPositions.y, {recenter: false});
+        // actor.data.update({
+        //     rotation: rotation
+        // });
+        // actor._object.updateSource(); //Updates local vision with rotation (token not rotated until saveMovement)
 
         //Send movements to backend on occasion
-        throttleSaveMovement(actor, tokenCenteredPositions, rotation, false);
+        // throttleSaveMovement(actor, tokenCenteredPositions, rotation, false);
         //Snap and save after not moving for a while
         debouncedSaveMovement(actor, tokenCenteredPositions, rotation);
     }
